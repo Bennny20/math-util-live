f077786cc4b6a00f68e0ef95f4d03d75dae275a1
Bennny20 <phientruong20@gmail.com>
2021-08-17 23:16:13 +0700

    Lan thu 2 up CODE

--- src/com/fu/mathutil/Demo1.java
+++ src/com/fu/mathutil/Demo1.java
@@ -16,8 +16,10 @@ public class Demo1 {
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = 120; actual: " +result);
         System.out.println("6! = 720; actual: " +MathUtil.getFactorial(6));
-        System.out.println("5! = 1; actual: " +MathUtil.getFactorial(0));
-        
+        System.out.println("0! = 1; actual: " +MathUtil.getFactorial(0));
+        // Them code sau lan dau tien lam chuyen ay 17/8/2021
+        System.out.println("1! = 1; actual: " +MathUtil.getFactorial(1));
+        System.out.println("3! = 6; actual: " +MathUtil.getFactorial(3));
         
         // Kĩ thuất kiểm thử phàn mềm: ước lượng giá trị trả về cửa hàm 
         // Expected value: 5! = 120 
